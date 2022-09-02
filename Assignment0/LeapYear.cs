0a7793a3fe93680e66a8ae1f8d88127cd47f4f4e
siar <siar@itu.dk>
2022-09-02 13:45:53 +0200

    Error handling

--- Assignment0/LeapYear.cs
+++ Assignment0/LeapYear.cs
@@ -6,6 +6,10 @@ public class LeapYear
     }
     public bool IsLeapYear(int year)
     {
+        if (year < 1582)
+        {
+            throw new Exception("Year must be over 1582");
+        }
         return year % 4 == 0 && !(year % 100 == 0 && !(year % 400 == 0));
     }
 }
--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -1,13 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 namespace Assignment0;
 class Program
 {
     private static void Main(string[] args)
     {
-        Console.Write("Enter year: ");    
-        int year = int.Parse(Console.ReadLine());
+        Console.Write("Enter year: ");
+        string input = Console.ReadLine();
+        int year;
+        bool isParsable = Int32.TryParse(input, out year);
         var ly = new LeapYear();
-        string output = ly.IsLeapYear(year) ? "yay" : "nay";
-        Console.WriteLine(output);
+        string output;
+
+        if (isParsable)
+        {
+            try
+            {
+                output = ly.IsLeapYear(year) ? "yay" : "nay";   
+            }
+            catch (System.Exception e)
+            {
+                output = e.Message;
+            }
+            Console.WriteLine(output);   
+        }else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
     }
 }
